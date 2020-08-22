550bc196316367ac344759a1f0795d1d7aaa02a9
John Dave Decano <johndavedecano@gmail.com>
2020-08-22 09:00:06 +0800

    update bucket name

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket"
+  bucket = "sveltejsbucketphilippines2020"
   acl    = "private"
   tags = {
     Name        = "My bucket"
